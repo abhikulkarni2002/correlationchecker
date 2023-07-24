f56c19327341f8e1f7b5ea4b36112ad3e1ffb7bd
Abhijeet Kulkarni <abhijeet.kulkarni.c@leewardenergy.com>
2023-07-24 12:23:12 -0500

    nice visual with finalDeliv

--- correlationChecker.py
+++ correlationChecker.py
@@ -1,15 +1,12 @@
 import scipy.stats as stats
 import pandas as pd
-import matplotlib.pyplot as plt
 
  
-df = pd.read_csv("C:\\Users\\Abhijeet.Kulkarni\\Downloads\\W11_202221_2022223_10 Minutes.csv")
+df = pd.read_csv("C:\\Users\\Abhijeet.Kulkarni\\Downloads\\202365_202375_10 Minutes.csv")
 windSpeed = df['NacWSpeedMean_mps'][0:2269].values
 transfTempWind = df['TransformerTempWind3_Mean'][0:2269].values
   
 correlation_coefficient, p_value = stats.pearsonr(windSpeed, transfTempWind)
-plt.scatter(windSpeed, transfTempWind, c='red')
-plt.show()
 
 print("Correlation coefficient:", correlation_coefficient)
 print("P-value", p_value)
